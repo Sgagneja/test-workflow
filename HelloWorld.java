96bafc4abeb1825917ac6984a1addfe3b7f8989d
Sgagneja <47675389+Sgagneja@users.noreply.github.com>
2021-11-17 14:54:28 -0600

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+// Outputs random number and arguments
+
+class HelloWorld {
+  public static void main(String... args) {
+    System.out.printf("Hello %s.%n", args.length == 0 ? "Java" : args[0]);
+    var gaussian = new Random().nextGaussian();
+    System.out.println("::set-output name=random-number::" + gaussian);
+    System.out.println("Goodbye and have fun with: " + gaussian);
+  }
+}
